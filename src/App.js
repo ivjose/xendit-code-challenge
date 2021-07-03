7e16e5971e983c049490e7b82382208b35ad2639
ivjose <ivjosesantos@gmail.com>
2021-07-03 15:29:54 +0800

    Fixed husky and lintstage setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const data = {
-    sample: '',
-  }
   return (
     <div className="App">
       <header className="App-header">
